272a529020f6c57412b6f936a2b0143ab3d801a9
habtesellasie <havecodesoft@gmail.com>
2024-05-18 00:51:01 +0300

    Add header

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: 'Habte',
+  url: 'https://portfolio-project.com',
+  description: 'Nextjs 14 Portfolio website using velite, tailwind and shadcn',
+  author: 'Habtesellasie Fissha',
+  links: {
+    twitter: 'https://twitter.com/havecodesoft',
+    linkedin: 'https://linkedin.com/in/habtesellasie',
+    github: 'https://github.com/habtesellasie',
+    personalSite: '',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
