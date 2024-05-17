2754d4a1df1ee0f8a4895790114ad9b7701f0318
habtesellasie <havecodesoft@gmail.com>
2024-05-18 02:24:58 +0300

    Add Home link to nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -9,28 +9,32 @@ import { cn } from '@/lib/utils';
 export function MainNav() {
   const pathname = usePathname();
   return (
-    <nav className='flex items-center space-x-4 lg:space-x-6'>
+    <nav className='flex items-center space-x-2 lg:space-x-4'>
       <Link href='/' className='mr-2 flex items-center space-x-2'>
         <Icons.logo className='h-6 w-6' />
         <span className='font-bold'>{siteConfig.name}</span>
       </Link>
       <Link
+        href='/'
+        className={cn(
+          'text-sm font-medium transition-colors py-2 px-4 hover:bg-gray-200 hover:text-primary rounded',
+          pathname === '/'
+            ? 'text-foreground bg-gray-200'
+            : 'text-foreground/60'
+        )}
+      >
+        Home
+      </Link>
+      <Link
         href='/blog'
         className={cn(
-          'text-sm font-medium transition-colors hover:text-primary',
-          pathname === '/blog' ? 'text-foreground' : 'text-foreground/60'
+          'text-sm font-medium transition-colors py-2 px-4 hover:bg-gray-200 hover:text-primary rounded',
+          pathname === '/blog'
+            ? 'text-foreground bg-gray-200 '
+            : 'text-foreground/60'
         )}
       >
         Blog
-      </Link>
-      <Link
-        href='/about'
-        className={cn(
-          'text-sm font-medium transition-colors hover:text-primary',
-          pathname === '/about' ? 'text-foreground' : 'text-foreground/60'
-        )}
-      >
-        About
       </Link>
     </nav>
   );
