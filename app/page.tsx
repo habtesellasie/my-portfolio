49288830b8a0b0b5ac5af5de8e50aa52fb2361ff
habtesellasie <havecodesoft@gmail.com>
2024-05-19 12:52:35 +0300

    Home Page Finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,239 @@
 import { buttonVariants } from '@/components/ui/button';
-import { siteConfig } from '@/config/site';
+import { certificates, projects, siteConfig } from '@/config/site';
 import { cn, formatDate, sortPosts } from '@/lib/utils';
 import { posts } from '#site/content';
 import Link from 'next/link';
 import { PostItem } from '@/components/post-item';
 import Image from 'next/image';
-import { Calendar } from 'lucide-react';
+import {
+  ArrowRightIcon,
+  Calendar,
+  Download,
+  Mail,
+  PhoneCall,
+} from 'lucide-react';
+import { Icons } from '@/components/icons';
 
 export default function Home() {
   const latestPosts = sortPosts(posts).slice(0, 5);
   return (
     <>
-      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32'>
-        <div className='container flex flex-col gap-4 text-center bg-transparent'>
-          <div className='size-[150px] rounded-full overflow-hidden mx-auto'>
-            <Image
-              width={300}
-              height={300}
-              className='w-full h-full object-cover'
-              src='/static/hab.jpeg'
-              alt='hero image'
-            />
-          </div>
-          <h1 className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance'>
-            I&apos;m Hab
-          </h1>
-          <p className='max-w-[42rem] mx-auto text-muted-foreground sm:text-xl inline-block py-4 text-pretty'>
-            A skilled web developer with expertise in{' '}
-            <span className='text-[#F55247] font-bold'>Laravel</span>,{' '}
-            <span className='text-[#61DBFB] font-bold'>React</span>, and{' '}
-            <span className='bg-gradient-to-r from-[#F55247] to-[#61DBFB] inline-block text-transparent bg-clip-text font-bold'>
-              UI/UX design
-            </span>
-            . I specialize in creating intuitive, dynamic, and visually
-            compelling web applications. My goal is to turn complex problems
-            into elegant digital solutions that drive success. Let&apos;s build
-            something extraordinary together.
-          </p>
-          <div className='flex flex-col gap-4 justify-center sm:flex-row'>
-            <Link
-              href='/blog'
-              className={cn(buttonVariants({ size: 'lg' }), 'w-full sm:w-fit')}
-            >
-              View my blog
-            </Link>
-            <Link
-              href={siteConfig.links.github}
-              target='_blank'
-              rel='noreferrer'
-              className={cn(
-                buttonVariants({ variant: 'outline', size: 'lg' }),
-                'w-full sm:w-fit'
-              )}
-            >
-              GitHub
-            </Link>
+      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32 mb-10'>
+        <div className='relative container flex flex-col gap-4 text-center bg-transparent'>
+          <div className='bg-[#df5b73] -translate-x-1/2 left-1/2 md:size-[450px] size-[300px] blur-[200px]  md:blur-[400px] -z-2 rounded-full absolute'></div>
+          <div className='relative z-2'>
+            <div className='size-[150px] rounded-full overflow-hidden mx-auto my-4'>
+              <Image
+                width={300}
+                height={300}
+                className='w-full h-full object-cover'
+                src='/static/hab.jpeg'
+                alt='hero image'
+              />
+            </div>
+            <h1 className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance z-3'>
+              I&apos;m Hab
+            </h1>
+            <p className='max-w-[42rem] mx-auto text-muted-foreground sm:text-xl inline-block py-4 text-pretty'>
+              A skilled web developer with expertise in{' '}
+              <span className='dark:text-[#61DBFB] text-[#287588] font-bold'>
+                React
+              </span>
+              , <span className='text-[#F55247] font-bold'>Laravel</span>, and{' '}
+              <span className='bg-gradient-to-r dark:to-[#F55247] to-[#F55247] dark:from-[#61DBFB] from-[#287588] inline-block text-transparent bg-clip-text font-bold'>
+                UI/UX design
+              </span>
+              . I specialize in creating intuitive, dynamic, and visually
+              compelling web applications. My goal is to turn complex problems
+              into elegant digital solutions that drive success.
+              <span className='block mt-4'>
+                Let&apos;s build something extraordinary together.
+              </span>
+            </p>
+            <div className='flex flex-col gap-4 justify-center sm:flex-row'>
+              <Link
+                href='/blog'
+                className={cn(
+                  buttonVariants({ size: 'lg' }),
+                  'w-full sm:w-fit'
+                )}
+              >
+                View my blog
+              </Link>
+              <Link
+                href={siteConfig.links.github}
+                target='_blank'
+                rel='noreferrer'
+                className={cn(
+                  buttonVariants({ variant: 'outline', size: 'lg' }),
+                  'w-full sm:w-fit'
+                )}
+              >
+                GitHub
+              </Link>
+            </div>
           </div>
         </div>
       </section>
       <section className='container py-4 flex flex-col space-y-6'>
-        <h2 className='text-2xl sm:text-4xl md:text-5xl font-black text-center'>
+        <h2 className='text-2xl mb-4 sm:text-3xl font-black text-center'>
           My Projects
         </h2>
+        <div className='space-y-40 max-lg:space-y-50'>
+          {projects.map((project, projIndex) => {
+            return (
+              <div
+                key={projIndex}
+                className='flex max-lg:flex-col items-center lg:even:flex-row-reverse gap-10 group justify-center'
+              >
+                <div className='flex flex-col justify-center gap-2'>
+                  <div className='flex flex-col justify-center w-full max-w-[600px] gap-4'>
+                    <h3 className='text-xl font-bold md:text-2xl'>
+                      {project.title}
+                    </h3>
+                    <p className='md:text-lg lg:text-xl text-md text-pretty'>
+                      {project.description}
+                    </p>
+                  </div>
+                  <div className='flex gap-2 flex-wrap mt-4'>
+                    {project.usedTechnology.map((tech, i) => {
+                      return (
+                        <span
+                          key={i}
+                          className='px-4 py-1 bg-gray-200 dark:bg-gray-700 rounded-full text-sm'
+                        >
+                          {tech}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+
+                <div className='w-full max-w-[600px] relative'>
+                  <Image
+                    width={300}
+                    height={500}
+                    className='w-full h-full rounded-lg object-contain '
+                    src={project.desktopImage}
+                    alt={project.title}
+                  />
+                  {project.mobileImage ? (
+                    <Image
+                      width={300}
+                      height={300}
+                      className='absolute right-1 max-sm:w-[200px] lg:group-even:left-1 -top-10 rounded-xl w-[250px] shadow-md '
+                      src={project.mobileImage}
+                      alt={project.title}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
       <section className='container py-4 flex flex-col space-y-6'>
-        <h2 className='text-2xl sm:text-4xl md:text-5xl font-black text-center'>
+        <h2 className='text-2xl sm:text-3xl font-black text-center'>
+          Certifications
+        </h2>
+        <div className='flex lg:flex-col gap-4 flex-wrap justify-center w-fit lg:mx-auto'>
+          <p className='text-sm text-gray-400'>
+            If the verification links do not work
+            <a
+              href='mailto:havecodesoft@gmail.com'
+              className='font-bold hover:text-blue-500'
+            >
+              {' '}
+              email me
+            </a>
+            .
+          </p>
+          {certificates.map((certificate, certIndex) => {
+            return certificate.status === 'Completed' ? (
+              <div
+                key={certIndex}
+                className='max-w-[420px] lg:max-w-[unset] shadow-lg dark:shadow-gray-200/20 ring-1 rounded-lg ring-gray-200 px-4 py-2 lg:flex lg:gap-4 lg:items-start lg:py-4 '
+              >
+                <h3 className='inline-flex gap-2 my-2 text-balance items-center lg:w-full'>
+                  {<Icons.certification_badge className='size-14' />}
+                  <span className='inline-flex flex-col'>
+                    <p className='text-xl font-bold inline-block lg:max-w-[230px] w-full'>
+                      {certificate.name}
+                    </p>
+                    <span className='text-md font-medium text-gray-600 dark:text-gray-300'>
+                      By {certificate.instructor}
+                    </span>
+                  </span>
+                </h3>
+                <div className='max-w-[600px]'>
+                  <p className='text-pretty'>{certificate.description}</p>
+                  <div className='flex items-center mt-4 mb-2 gap-4'>
+                    <Link
+                      href={certificate.link}
+                      target='_blank'
+                      className='text-blue-500 dark:text-blue-400 hover:underline transition underline-offset-2 inline-block lg:mb-0'
+                    >
+                      verification link
+                    </Link>
+                    <a
+                      href={certificate.file}
+                      target='_blank'
+                      className='group transition'
+                      // download={certificate.file}
+                    >
+                      <Download
+                        size={20}
+                        className='group-hover:text-blue-500'
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className='max-w-[420px] lg:max-w-[100%] shadow-lg dark:shadow-gray-200/20 ring-1 rounded-lg ring-gray-200 px-4 py-2 lg:flex lg:gap-4 lg:items-start lg:py-4'>
+                <h3 className='inline-flex gap-2 my-2 text-balance items-center lg:w-full'>
+                  {<Icons.certification_badge className='size-14' />}
+                  <span className='inline-flex flex-col'>
+                    <p className='text-xl font-bold inline-block lg:max-w-[230px] w-full'>
+                      {certificate.name}
+                    </p>
+                    <span className='text-md font-medium text-gray-600 dark:text-gray-300'>
+                      By {certificate.instructor}
+                    </span>
+                  </span>
+                </h3>
+                <div className='max-w-[600px] w-full'>
+                  <div className='inline-flex flex-col gap-2 w-full'>
+                    <p className='relative block rounded-md w-full h-[200px] lg:w-[600px] dark:bg-gray-700 bg-gray-200'>
+                      <span className='inline-block absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 -rotate-[20deg] font-bold dark:text-gray-400 text-gray-400'>
+                        Comming Soon . . .
+                      </span>
+                    </p>
+                  </div>
+                  <p className='text-blue-500 dark:text-blue-400 hover:underline transition underline-offset-2 inline-block mt-2 mb-2 lg:mb-0 bg-blue-500 w-32 h-3 rounded-full'></p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section className='container py-4 flex flex-col space-y-6'>
+        <h2 className='text-2xl sm:text-3xl font-black text-center'>
           Latest Posts
         </h2>
         <div className='flex flex-wrap items-stretch justify-center gap-4 '>
           {latestPosts.map((post) => {
             return (
-              <Link
-                href={post.slug}
+              <div
                 key={post.slug}
                 className='flex flex-col justify-between gap-2 py-4 rounded px-4 shadow hover:bg-gray-200 w-full max-w-[450px] ring-[1px] ring-gray-100 hover:ring-none transition dark:hover:bg-gray-700 dark:ring-gray-500'
               >
                 <div>
-                  <div>
-                    <h2 className='text-2xl font-bold'>
-                      <Link href={post.slug}>{post.title}</Link>
-                    </h2>
-                  </div>
+                  <h2 className='text-2xl font-bold'>
+                    <Link href={post.slug}>{post.title}</Link>
+                  </h2>
                   <div className='max-w-none text-muted-foreground'>
                     {post.description}
                   </div>
@@ -93,17 +246,94 @@ export default function Home() {
                       <time dateTime={post.date}>{formatDate(post.date)}</time>
                     </dd>
                   </dl>
-                  <span>Read more →</span>
+                  <Link href={post.slug} className='hover:underline'>
+                    Read more →
+                  </Link>
                 </div>
-              </Link>
+              </div>
             );
           })}
         </div>
       </section>
       <section className='container py-4 flex flex-col space-y-6'>
-        <h2 className='text-2xl sm:text-4xl md:text-5xl font-black text-center'>
+        <h2 className='text-2xl sm:text-3xl font-black text-center'>
           Contact me
         </h2>
+        <div className='sm:flex justify-center mx-auto items-center gap-5'>
+          <Image
+            width={300}
+            height={500}
+            className='w-full max-w-[400px] rounded-lg object-contain '
+            src='/static/contact.jpg'
+            alt='Contact Me'
+          />
+
+          <div className='flex flex-col gap-2 sm:mt-0 mt-4 max-sm:items-center'>
+            <a
+              className='flex items-center gap-2 transition hover:text-blue-500 ml-2'
+              href='tel:0945596959'
+            >
+              <PhoneCall size={20} />
+              +251 945 596 959
+            </a>
+            <a
+              className='flex items-center gap-2 transition hover:text-blue-500 ml-2'
+              href='mailto:havecodesoft@gmail.com'
+            >
+              <Mail size={20} />
+              havecodesoft@gmail.com
+            </a>
+            <nav className='flex items-center'>
+              <Link
+                href={siteConfig.links.linkedin}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div
+                  className={cn(
+                    buttonVariants({ variant: 'ghost' }),
+                    'w-10 px-0'
+                  )}
+                >
+                  <Icons.linkedin className='size-6' />
+                  <span className='sr-only'>Linkedin</span>
+                </div>
+              </Link>
+              <Link
+                href={siteConfig.links.github}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div
+                  className={cn(
+                    buttonVariants({ variant: 'ghost' }),
+                    'w-10 px-0'
+                  )}
+                >
+                  <Icons.gitHub className='h-4 w-4' />
+                  <span className='sr-only'>GitHub</span>
+                </div>
+              </Link>
+              <Link
+                href={siteConfig.links.twitter}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div
+                  className={cn(
+                    buttonVariants({ variant: 'ghost' }),
+                    'w-10 px-0'
+                  )}
+                >
+                  <Icons.twitter className='h-4 w-4' />
+                  <span className='sr-only'>Twitter</span>
+                </div>
+              </Link>
+              {/* <ModeToggle /> */}
+              {/* <MobileNav /> */}
+            </nav>
+          </div>
+        </div>
       </section>
     </>
   );
--- config/site.ts
+++ config/site.ts
@@ -13,4 +13,142 @@ export const siteConfig = {
   },
 };
 
+export const projects = [
+  {
+    title: 'Apple Website Clone',
+    description:
+      'The "Apple Website Clone" project aims to replicate the user interface and functionality of the official Apple website using modern web development technologies By meticulously recreating the layout, design elements, and features of the Apple site, this project serves as an excellent opportunity to enhance your HTML, CSS, JavaScript, and React skills. Additionally, it provides hands-on experience in responsive design techniques, ensuring optimal viewing across various devices.',
+    link: 'https://appledottcom.netlify.app',
+    desktopImage: '/static/project/Appledotcom.png',
+    mobileImage: '/static/project/Appledotcom-mobile.png',
+    usedTechnology: ['HTML5', 'SCSS', 'React', 'Git'],
+  },
+  // {
+  //   title: 'Donkey Tube',
+  //   description:
+  //     'The Donkey Tube Website is to provide an immersive multimedia platform catering to audiences seeking diverse content in English and Amharic languages With a focus on seamless language selection, real-time YouTube integration, and elegant dark and light theme options, Donkey Tube aims to redefine the digital media landscape. Utilizing React.js for dynamic user interfaces and responsive design, we seamlessly implemented language selection, YouTube integration via the YouTube API, and dark/light theme options.',
+  //   link: 'https://donkeytube.netlify.app',
+  //   desktopImage: '/static/project/Donkey-tube.png',
+  //   mobileImage: '/static/project/Donkey-tube-mobile.png',
+  //   usedTechnology: [
+  //     'UI/UX Design',
+  //     'Figma',
+  //     'HTML5',
+  //     'SCSS',
+  //     'React',
+  //     'Youtube api',
+  //     'Git',
+  //   ],
+  // },
+  {
+    title: 'Laba Media',
+    description:
+      "I had the opportunity to develop a comprehensive and dynamic website for Laba Media & Communications, a renowned media firm celebrated for its versatility and extensive experience across diverse sectors. Leveraging React, SCSS, SPA, and PWA technologies, I created an engaging and responsive platform that showcases Laba Media's rich history of collaborations with local and international NGOs.",
+    link: 'https://labamedia.netlify.app',
+    desktopImage: '/static/project/Laba-Media.png',
+    mobileImage: '/static/project/Laba-Media-mobile.png',
+    usedTechnology: ['HTML5', 'Figma', 'SCSS', 'PWA', 'SPA', 'React', 'Git'],
+  },
+  {
+    title: 'Cinematographer Portfolio',
+    description:
+      "Embark on a visual odyssey with Ephrem, where creativity meets innovation in a multimedia showcase designed to captivate and inspire Ephrem's goal for this project was to create an immersive portfolio that highlights his expertise as a cinematographer, editor, and graphics designer, while also providing a seamless and engaging user experience.",
+    link: 'https://tanchiwedia.netlify.app',
+    desktopImage: '/static/project/Tanchiwedia.png',
+    mobileImage: '/static/project/Tanchiwedia-mobile.png',
+    usedTechnology: ['HTML5', 'Figma', 'CSS3', 'JavaScript', 'Git'],
+  },
+  {
+    title: 'Basic Typing Test',
+    description:
+      'I developed a user-friendly and interactive Simple Typing Test application utilizing HTML5, CSS3, and JavaScript. This web application is designed to help users improve their typing speed and accuracy through a clean and intuitive interface. Key features include real-time feedback, a dynamic word list, and a timer to track progress. The project demonstrates my proficiency in front-end development and my ability to create engaging and functional web applications.',
+    link: 'https://testyping.netlify.app/',
+    desktopImage: '/static/project/Test-typing.png',
+    mobileImage: '',
+    usedTechnology: ['HTML5', 'CSS3', 'JavaScript', 'Git'],
+  },
+];
+
+export const certificates = [
+  {
+    name: 'The Frontend Developer Career Path',
+    course: '12',
+    description:
+      'I completed the Frontend Developer Career Path, a comprehensive program designed to make me a hireable frontend developer and excel in job interviews. This extensive program included over 70 hours of top-notch tutorials, hundreds of coding challenges, and numerous real-world projects, providing a robust and hands-on learning experience. The career path is structured into 12 interactive modules, ensuring consistent progress and retention of key concepts. Throughout the program, I mastered HTML, CSS, JavaScript, React, UI design, and career strategy, equipping me with the skills and confidence to succeed in the competitive field of frontend development.',
+    link: 'https://scrimba.com/certificate/uB5B7DSx/gfrontend',
+    instructor: 'Scrimba',
+    status: 'Completed',
+    file: '/static/certificates/frontend-developer-career-path-certificate.pdf',
+  },
+  {
+    name: 'The AI Engineer Path',
+    course: '1',
+    description:
+      'I completed the AI Engineer Path, a cutting-edge program focused on building applications powered by generative AI—an essential skill for 2024 and beyond, highly sought after by product teams at startups, agencies, and large corporations. This program equipped me with the knowledge and hands-on experience needed to integrate generative AI into various applications, enhancing their functionality and user experience. Through this path, I gained proficiency in the latest AI technologies and methodologies, positioning myself at the forefront of the evolving AI landscape.',
+    link: 'https://scrimba.com/certificate/uB5B7DSx/gaiengineer',
+    instructor: 'Scrimba',
+    status: 'Completed',
+    file: '/static/certificates/ai-engineer-path-certificate.pdf',
+  },
+  {
+    name: 'Learn Responsive Web Design',
+    course: '1',
+    description:
+      'I completed a comprehensive course in responsive web design, which elevated my skills to a professional level. This course focused on building advanced layouts and solving engaging coding challenges, ensuring a deep understanding of responsive design principles. Through hands-on projects and real-world scenarios, I learned to create websites that provide seamless user experiences across various devices and screen sizes, solidifying my expertise in responsive web design.',
+    link: 'https://scrimba.com/certificate/uB5B7DSx/gresponsive',
+    instructor: 'Scrimba',
+    status: 'Completed',
+    file: '/static/certificates/responsive-web-design-certificate.pdf',
+  },
+  {
+    name: 'JavaScript Deep Dive',
+    course: '1',
+    description:
+      'I completed the JavaScript Deep Dive course, which provided an in-depth, systematic exploration of advanced JavaScript concepts from the ground up. This course featured comprehensive walkthroughs of the most critical aspects of the language, enhancing my understanding and proficiency in JavaScript. As part of the hands-on learning experience, I built a Google Keep clone and a Hacker News clone using vanilla JavaScript, demonstrating my ability to apply advanced JavaScript techniques to real-world projects.',
+    link: 'https://scrimba.com/certificate/uB5B7DSx/gjavascript',
+    instructor: 'Scrimba',
+    status: 'Completed',
+    file: '/static/certificates/javascript-deep-dive-certificate.pdf',
+  },
+  {
+    name: 'Learn UI Design',
+    course: '1',
+    description:
+      'I completed a comprehensive UI design course created by Gary Simon, a leading online instructor in the field. This course taught me to build beautiful and well-functioning user interfaces using HTML and CSS. Packed with interactive tutorials, the course ensured that I retained and applied the knowledge effectively. Through this training, I honed my skills in crafting visually appealing and highly functional user interfaces, enhancing my ability to deliver exceptional web design projects.',
+    link: 'https://scrimba.com/certificate/uB5B7DSx/gdesignbootcamp',
+    instructor: 'Scrimba',
+    status: 'Completed',
+    file: '/static/certificates/ui-design-certificate.pdf',
+  },
+
+  {
+    name: 'Google UX Design',
+    course: '7',
+    description:
+      'I earned the Google UX Design Professional Certificate by completing seven rigorous courses developed by Google. This program included hands-on, practice-based assessments designed to prepare me for introductory-level roles in UX design. Throughout the coursework, I mastered the complete design process, from empathizing with users and defining their pain points to ideating design solutions, creating wireframes and prototypes, and testing designs to gather valuable feedback. This certification has equipped me with the skills and knowledge to create user-centered designs that enhance the overall user experience.',
+    link: 'https://coursera.org/verify/professional-cert/4MSYH4QNLDAB',
+    instructor: 'Google',
+    status: 'Completed',
+    file: '/static/certificates/google-ux-design.pdf',
+  },
+  {
+    name: 'Meta Frontend Developer',
+    course: '9',
+    description: '--',
+    link: '',
+    instructor: 'Meta',
+    status: 'Comming soon',
+    file: '/static/certificates/',
+  },
+  {
+    name: 'IBM Frontend Developer',
+    course: '11',
+    description: '--',
+    link: '',
+    instructor: 'IBM',
+    status: 'Comming soon',
+    file: '/static/certificates/',
+  },
+];
+
 export type SiteConfig = typeof siteConfig;
